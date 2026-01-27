4b935e85ade0e1537b465f2380cd735f1268c5b6
luyao787 <zly9706@gmail.com>
2026-01-27 20:55:10 +0800

    comment

--- ilqrx/lqr.py
+++ ilqrx/lqr.py
@@ -84,92 +84,92 @@ def lqr_dual_variables(Ps, ps, xs):
 
 
 
-@jax.jit
-def lqr_regularized_step(Q, R, S, q, r, P, p, A, B, c, U, mu):
+# @jax.jit
+# def lqr_regularized_step(Q, R, S, q, r, P, p, A, B, c, U, mu):
     
-    symmetrize = lambda x: (x + x.T) / 2
+#     symmetrize = lambda x: (x + x.T) / 2
 
-    nx = Q.shape[0]
-    I_nx = jnp.eye(nx)
-    chofac_G = (U, False) 
-    P_hat = jsp.linalg.cho_solve(chofac_G, P)
-    p_hat = jsp.linalg.cho_solve(chofac_G, P @ c + p)
+#     nx = Q.shape[0]
+#     I_nx = jnp.eye(nx)
+#     chofac_G = (U, False) 
+#     P_hat = jsp.linalg.cho_solve(chofac_G, P)
+#     p_hat = jsp.linalg.cho_solve(chofac_G, P @ c + p)
 
-    AtP = jnp.matmul(A.T, P_hat)
-    AtPA = jnp.matmul(AtP, A)
-    BtP = jnp.matmul(B.T, P_hat)
-    BtPA = jnp.matmul(BtP, A)
+#     AtP = jnp.matmul(A.T, P_hat)
+#     AtPA = jnp.matmul(AtP, A)
+#     BtP = jnp.matmul(B.T, P_hat)
+#     BtPA = jnp.matmul(BtP, A)
     
-    Qxx = Q + AtPA
-    Quu = R + jnp.matmul(BtP, B)
-    Qux = S + BtPA  # u^T @ S @ x
+#     Qxx = Q + AtPA
+#     Quu = R + jnp.matmul(BtP, B)
+#     Qux = S + BtPA  # u^T @ S @ x
     
-    Qx  = q + jnp.matmul(A.T, p_hat)
-    Qu  = r + jnp.matmul(B.T, p_hat)
+#     Qx  = q + jnp.matmul(A.T, p_hat)
+#     Qu  = r + jnp.matmul(B.T, p_hat)
         
-    chofac = jsp.linalg.cho_factor(Quu)
-    K = -jsp.linalg.cho_solve(chofac, Qux)
-    k = -jsp.linalg.cho_solve(chofac, Qu).squeeze()
+#     chofac = jsp.linalg.cho_factor(Quu)
+#     K = -jsp.linalg.cho_solve(chofac, Qux)
+#     k = -jsp.linalg.cho_solve(chofac, Qu).squeeze()
     
-    P = symmetrize(Qxx + Qux.T @ K)
-    p = Qx + jnp.matmul(Qux.T, k)
+#     P = symmetrize(Qxx + Qux.T @ K)
+#     p = Qx + jnp.matmul(Qux.T, k)
 
-    G = I_nx  + mu * P
-    U, _ = jsp.linalg.cho_factor(G)
+#     G = I_nx  + mu * P
+#     U, _ = jsp.linalg.cho_factor(G)
 
-    return K, k, P, p, U
+#     return K, k, P, p, U
 
-@jax.jit
-def lqr_regularized_backward(Qs, qs, Rs, rs, Ss, As, Bs, cs, mu):
+# @jax.jit
+# def lqr_regularized_backward(Qs, qs, Rs, rs, Ss, As, Bs, cs, mu):
     
-    T = Qs.shape[0] - 1
-    n = Qs.shape[1]
-    m = Rs.shape[1]
+#     T = Qs.shape[0] - 1
+#     n = Qs.shape[1]
+#     m = Rs.shape[1]
 
-    Ps = jnp.zeros((T + 1, n, n))
-    ps = jnp.zeros((T + 1, n))
-    Ks = jnp.zeros((T, m, n))
-    ks = jnp.zeros((T, m))
-    Us = jnp.zeros((T + 1, n, n))
+#     Ps = jnp.zeros((T + 1, n, n))
+#     ps = jnp.zeros((T + 1, n))
+#     Ks = jnp.zeros((T, m, n))
+#     ks = jnp.zeros((T, m))
+#     Us = jnp.zeros((T + 1, n, n))
 
-    Ps = Ps.at[-1].set(Qs[T])
-    ps = ps.at[-1].set(qs[T])
+#     Ps = Ps.at[-1].set(Qs[T])
+#     ps = ps.at[-1].set(qs[T])
 
-    G_T = jnp.eye(n) + mu * Qs[T]
-    U_T, _ = jsp.linalg.cho_factor(G_T)
-    Us = Us.at[T].set(U_T)
+#     G_T = jnp.eye(n) + mu * Qs[T]
+#     U_T, _ = jsp.linalg.cho_factor(G_T)
+#     Us = Us.at[T].set(U_T)
     
-    def body(tt, inputs):
-        Ks, ks, Ps, ps, Us = inputs         
-        t = T - tt - 1
-        K_t, k_t, P_t, p_t, U_t = \
-            lqr_regularized_step(Qs[t], Rs[t], Ss[t], qs[t], rs[t], Ps[t+1], ps[t+1], As[t], Bs[t], cs[t], Us[t+1], mu)
+#     def body(tt, inputs):
+#         Ks, ks, Ps, ps, Us = inputs         
+#         t = T - tt - 1
+#         K_t, k_t, P_t, p_t, U_t = \
+#             lqr_regularized_step(Qs[t], Rs[t], Ss[t], qs[t], rs[t], Ps[t+1], ps[t+1], As[t], Bs[t], cs[t], Us[t+1], mu)
         
-        Ks = Ks.at[t].set(K_t)
-        ks = ks.at[t].set(k_t)
-        Ps = Ps.at[t].set(P_t)
-        ps = ps.at[t].set(p_t)
-        Us = Us.at[t].set(U_t)
+#         Ks = Ks.at[t].set(K_t)
+#         ks = ks.at[t].set(k_t)
+#         Ps = Ps.at[t].set(P_t)
+#         ps = ps.at[t].set(p_t)
+#         Us = Us.at[t].set(U_t)
         
-        return Ks, ks, Ps, ps, Us
+#         return Ks, ks, Ps, ps, Us
         
-    return lax.fori_loop(0, T, body, (Ks, ks, Ps, ps, Us)) 
+#     return lax.fori_loop(0, T, body, (Ks, ks, Ps, ps, Us)) 
 
-@jax.jit
-def lqr_regularized_forward(x0, Ks, ks, As, Bs, cs, Us, ps, mu):
-    T, m, n = Ks.shape
-    xs = jnp.zeros((T + 1, n))
-    us = jnp.zeros((T, m))
-    xs = xs.at[0].set(x0)
+# @jax.jit
+# def lqr_regularized_forward(x0, Ks, ks, As, Bs, cs, Us, ps, mu):
+#     T, m, n = Ks.shape
+#     xs = jnp.zeros((T + 1, n))
+#     us = jnp.zeros((T, m))
+#     xs = xs.at[0].set(x0)
     
-    def body(t, inputs):
-        xs, us = inputs
-        u_t = Ks[t] @ xs[t] + ks[t]
-        x_tmp = As[t] @ xs[t] + Bs[t] @ u_t + cs[t] - mu * ps[t+1]
-        x_t1 = jsp.linalg.cho_solve((Us[t+1], False), x_tmp)
-        us = us.at[t].set(u_t)
-        xs = xs.at[t+1].set(x_t1)
+#     def body(t, inputs):
+#         xs, us = inputs
+#         u_t = Ks[t] @ xs[t] + ks[t]
+#         x_tmp = As[t] @ xs[t] + Bs[t] @ u_t + cs[t] - mu * ps[t+1]
+#         x_t1 = jsp.linalg.cho_solve((Us[t+1], False), x_tmp)
+#         us = us.at[t].set(u_t)
+#         xs = xs.at[t+1].set(x_t1)
 
-        return xs, us
+#         return xs, us
     
-    return lax.fori_loop(0, T, body, (xs, us))
+#     return lax.fori_loop(0, T, body, (xs, us))
